82dac3587e270a7a885fdb4c822661ccfa1c3fca
Arjan van Wijk <thanarie@gmail.com>
2019-10-25 11:30:16 +0200

    Correct the Query filter typings to only allow an object

--- src/lib/ApiQuery.ts
+++ src/lib/ApiQuery.ts
@@ -38,6 +38,10 @@ export type ApiQuerySortParameter<R extends AnyResource> = Array<
   ApiSortRule<ResourceAttributeNames<R>>
 >
 
+export type ApiQueryFilterParameter = {
+  [key: string]: ApiQueryParameterValue
+}
+
 export type ApiQueryIncludeParameter<
   R extends AnyResource
 > = BaseApiQueryIncludeParameters<R>
@@ -55,19 +59,19 @@ export type ApiQueryResourceParameters<R extends AnyResource> = Partial<{
   fields: BaseApiQueryFieldsParameter<R>
 }>
 
-export type ApiQueryFilterParameters<
+export type ApiQueryFiltersParameters<
   R extends AnyResource,
   S extends Partial<ApiSetup>
 > = Partial<{
   page: ApiQueryPageParameter<S>
   sort: ApiQuerySortParameter<R>
-  filter: ApiQueryParameter
+  filter: ApiQueryFilterParameter
 }>
 
 export type FetchQueryParameters<
   R extends AnyResource,
   S extends Partial<ApiSetup>
-> = ApiQueryResourceParameters<R> & ApiQueryFilterParameters<R, S>
+> = ApiQueryResourceParameters<R> & ApiQueryFiltersParameters<R, S>
 
 export type ApiQueryParameterValue =
   | string
