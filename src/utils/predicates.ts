8ab20e6aca2a35e759581569663eb867a5222701
Nathan Uphoff <nathan.uphoff@mediamonks.com>
2019-10-29 10:03:32 +0100

    Removed unused predicates

--- src/utils/predicates.ts
+++ src/utils/predicates.ts
@@ -1,12 +1,7 @@
-import { isObject, and, at, literal, Predicate } from 'isntnt'
+import { at, literal, Predicate } from 'isntnt'
 
 import { ResourceType } from '../lib/Resource'
 import { ResourceIdentifier } from '../lib/ResourceIdentifier'
-
-const has = <K extends PropertyKey>(key: K) =>
-  and(isObject, (value: any): value is Record<K, any> => key in value)
-
-export const hasData = has('data')
 
 export const createIsResourceOfType = <T extends ResourceType>(
   type: T,
