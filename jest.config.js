dd8e69cdac2c8cbde059e9e8abda64648599f0c0
Nathan <Nathan@MacBook-Pro-Nathan.local>
2020-02-12 20:15:19 +0100

    Attempt to use seperate test tsconfig

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   modulePaths: ['<rootDir>'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
+    Headers: class MockHeaders {
+      append() {}
+    },
+    Request: class MockRequest {},
+  },
 }
