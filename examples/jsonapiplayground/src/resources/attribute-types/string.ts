567647a0722b9e158a8e70ea624779cbeb404774
Nathan Uphoff <nathan.uphoff@mediamonks.com>
2020-07-20 16:57:17 +0200

    Remove jsonapiplayground example

--- examples/jsonapiplayground/src/resources/attribute-types/string.ts
+++
@@ -1,4 +0,0 @@
-import { isString } from 'isntnt'
-import Type from '../../../../../src/type'
-
-export const string = Type.is('a string', isString)
--- examples/jsonapiplayground/src/resources/attribute-types/uint.ts
+++
@@ -1,4 +0,0 @@
-import { isUint } from 'isntnt'
-import Type from '../../../../../src/type'
-
-export const uint = Type.is('an unsigned integer', isUint)
