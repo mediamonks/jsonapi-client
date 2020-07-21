e710d3b410fd1d21821484ceef45601127ff2254
Nathan Uphoff <nathan.uphoff@mediamonks.com>
2020-07-21 16:27:17 +0200

    Add ResourceIdentifier constructor guards, add ResourceIdentifier specs

---
+++ src/resource/identifier/index.spec.ts
@@ -0,0 +1,31 @@
+import { ResourceIdentifier } from '.'
+
+describe('ResourceIdentifier', () => {
+  describe('#constructor', () => {
+    it('must throw an Error if its first param (type) is not a string', () => {
+      expect(() => new ResourceIdentifier(null as any, 'id')).toThrowError()
+    })
+
+    it('must throw an Error if its first param (type) is an empty string', () => {
+      expect(() => new ResourceIdentifier('', 'id')).toThrowError()
+    })
+
+    it('must throw an Error if its first param (type) is not a valid ResourceType', () => {
+      expect(() => new ResourceIdentifier('-Type-', 'id')).toThrowError()
+    })
+
+    it('must set its first param (type) to ResourceIdentifier#type unmodified if it’s a valid ResourceType', () => {
+      const resourceIdentifier = new ResourceIdentifier('Type', 'id')
+      expect(resourceIdentifier.type).toBe('Type')
+    })
+
+    it('must throw an Error if its second param (id) is not a string', () => {
+      expect(() => new ResourceIdentifier('Type', null as any)).toThrowError()
+    })
+
+    it('must set its second param (id) to ResourceIdentifier#id unmodified if it’s a string', () => {
+      const resourceIdentifier = new ResourceIdentifier('Type', 'id')
+      expect(resourceIdentifier.id).toBe('id')
+    })
+  })
+})
--- src/resource/identifier/index.ts
+++ src/resource/identifier/index.ts
@@ -1,3 +1,6 @@
+import { isString, test, and, at, above } from 'isntnt'
+import Type from '../../type'
+
 import { ResourceId, ResourceType } from '../../types'
 
 export class ResourceIdentifier<T extends ResourceType> {
@@ -5,7 +8,22 @@ export class ResourceIdentifier<T extends ResourceType> {
   readonly id: ResourceId
 
   constructor(type: T, id: ResourceId) {
-    this.type = type
-    this.id = id
+    this.type = resourceType.parse(type) as T
+    this.id = resourceId.parse(id)
   }
 }
+
+// Type
+const string = Type.is('a string', isString)
+
+export const resourceFieldName = Type.is(
+  'a valid field name',
+  test(/^[^-_ ]([a-zA-Z0-9][^+,\.\[\]!"#$%&'\(\)\/*:;<=>?@\\^`{|}~]+)+[^-_ ]$/),
+).withCode('172')
+
+export const resourceType = resourceFieldName.with({
+  description: 'a valid resource type',
+  code: '162',
+})
+
+const resourceId = string.withCode('123')
