7b4deb33510d637f84c91569c43bbecee0cff27a
Nathan Uphoff <nathan.uphoff@mediamonks.com>
2020-07-24 11:03:09 +0200

    Make const names more explanatory

--- examples/jsonapiplayground/src/index.ts
+++ examples/jsonapiplayground/src/index.ts
@@ -1,6 +1,6 @@
 import JSONAPI, { ImplicitRelationshipData, AbsolutePathRoot } from 'jsonapi-client'
 
-import { author } from './resources'
+import { author, book } from './resources'
 
 const url = new URL('http://jsonapiplayground.reyesoft.com/v2')
 
@@ -9,48 +9,35 @@ const client = JSONAPI.client(url, {
   implicitRelationshipData: ImplicitRelationshipData.ResourceIdentifiers,
 })
 
-const authors = client.endpoint('authors', author)
+const authorEndpoint = client.endpoint('authors', author)
 
-authors.getOne('2').then((resource) => console.log(resource.data))
-
-authors
+authorEndpoint
   .create({
     name: 'Hans',
     date_of_birth: new Date(1970, 0, 1),
     birthplace: 'Netherlands',
   })
-  .then((author) => {
-    console.log(author.data.date_of_death) // null
+  .then((oneAuthor) => {
+    console.log(oneAuthor.data)
   })
 
-const authorWithBooks = author.filter(
+const authorFilter = author.filter(
   {
     [author.type]: ['name', 'books'],
   },
   { books: null },
 )
 
-authors.getOne('2', authorWithBooks).then((book) => console.log(book.data.name))
+authorEndpoint.getOne('2', authorFilter).then((oneBook) => {
+  console.log(oneBook.data)
+})
 
-authors.getMany(null, authorWithBooks).then((books) => console.log(books.data.length))
-
-const getAuthorsWithBooks = authors.many(authorWithBooks)
-
-getAuthorsWithBooks({
+const authorQuery = {
   page: {
     number: 1,
   },
-})
+}
 
-const getAuthorBooks = authors.toMany('books')
-
-getAuthorBooks('1').then((books) => {
-  console.log(books.data[0].date_published)
-  if (books.hasNextPage()) {
-    return getAuthorsWithBooks({
-      page: {
-        number: 2,
-      },
-    })
-  }
+authorEndpoint.getMany(authorQuery, authorFilter).then((manyBooks) => {
+  console.log(manyBooks.data)
 })
