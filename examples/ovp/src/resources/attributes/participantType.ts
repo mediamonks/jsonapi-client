92804475324c5fe6485f0d0a379e4e831e70fff9
Nathan Uphoff <nathan.uphoff@mediamonks.com>
2020-07-29 12:20:48 +0200

    Restore ParticipantType enum

--- examples/ovp/src/resources/attributes/participantType.ts
+++ examples/ovp/src/resources/attributes/participantType.ts
@@ -2,7 +2,7 @@ import { Type } from '../../../../../src'
 
 export enum ParticipantType {
   Team = 'TEAM',
-  // Individual = 'INDIVIDUAL',
+  Individual = 'INDIVIDUAL',
 }
 
 export const participantType: Type<ParticipantType> = Type.either(...Object.values(ParticipantType))
