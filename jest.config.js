a2521b0cba1f62d1695dbd497a13dc4613f4431f
Nathan Uphoff <nathan.uphoff@mediamonks.com>
2020-09-15 08:44:23 +0200

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
 module.exports = {
+  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  modulePaths: ['<rootDir>'],
+  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    'this-is-just-here-to-not-match-anything-and-make-sure-the-node-modules-are-not-ignored-in-the-transforms',
+  ],
+  setupFiles: [
+    '<rootDir>/test/fields.ts',
+    '<rootDir>/test/formatters.ts',
+    '<rootDir>/test/polyfills.ts',
+  ],
 }
